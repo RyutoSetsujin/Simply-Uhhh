db9c855c50a13847b9abf86e05bf300716cbd195
Horsey <cheval.de.jeanvaljean@gmail.com>
2021-05-29 00:08:52 -0700

    stepartist textbox more resembles SL 3.95, move PeakNPS
    -Replaces the stepartist quad with an ActorMultiVertex similar to the "COUNTS AS X ROUNDS" bubble. This asset is also longer, spanning the entire width of DensityGraph (length adjusted smaller to fit in 4:3).
    
    -Used the same animation of the stepartist bubble flying in/out as in SL3.95 .
    
    -Reorder the loading of StepsDisplayList and and PerPlayer so that the the stepartistbubble loads on top of the StepsDisplayList.
    
    -Append "Peak NPS" readout to the stream breakdown to allow for more room for the Stepartist field. (PeakNPS no longer appears to be "floating" above the NPS graph.)
    
    I originally wanted to stretch the stepartist bubble to be long enough to reach PeakNPS, but I couldn't figure out the layering conflicts whereby PeakNPS and the NPSgraph are in the same ActorFrame, yet PeakNPS would have to draw higher than the Stepartist bubble while the bubble also draws on top of the NPSgraph. Since draworder doesn't work to fix the layering conflict, the only way to fix this would be to completely merge the two.

--- BGAnimations/ScreenSelectMusic overlay/PerPlayer/DensityGraph.lua
+++ BGAnimations/ScreenSelectMusic overlay/PerPlayer/DensityGraph.lua
@@ -96,33 +96,6 @@ af2[#af2+1] = NPS_Histogram(player, width, height)..{
 -- We do this in parent actorframe because we want to "stall" before we parse.
 af2[#af2]["CurrentSteps"..pn.."ChangedMessageCommand"] = nil
 
--- The Peak NPS text
-af2[#af2+1] = LoadFont("Common Normal")..{
-	Name="NPS",
-	Text="Peak NPS: ",
-	InitCommand=function(self)
-		self:horizalign(left):zoom(0.8)
-		if player == PLAYER_1 then
-			self:addx(60):addy(-41)
-		else
-			self:addx(-136):addy(-41)
-		end
-
-		-- We want black text in Rainbow mode, white otherwise.
-		self:diffuse(ThemePrefs.Get("RainbowMode") and {0, 0, 0, 1} or {1, 1, 1, 1})
-	end,
-	HideCommand=function(self)
-		self:settext("Peak NPS: ")
-		self:visible(false)
-	end,
-	RedrawCommand=function(self)
-		if SL[pn].Streams.PeakNPS ~= 0 then
-			self:settext(("Peak NPS: %.1f"):format(SL[pn].Streams.PeakNPS * SL.Global.ActiveModifiers.MusicRate))
-			self:visible(true)
-		end
-	end,
-}
-
 -- Breakdown
 af2[#af2+1] = Def.ActorFrame{
 	Name="Breakdown",
@@ -155,12 +128,15 @@ af2[#af2+1] = Def.ActorFrame{
 		HideCommand=function(self)
 			self:settext("")
 		end,
+		--we're going to move the Peak NPS text to the beginning of the breakdown
+		--we need to do it this way because of layering conflicts and being unable to match the stepartist animation when the screen loads
+		--by moving PeakNPS here, there's more room for the Stepartist text
 		RedrawCommand=function(self)
 			local textZoom = 0.8
-			self:settext(GenerateBreakdownText(pn, 0))
+			self:settext(("Peak NPS: %.1f   "):format(SL[pn].Streams.PeakNPS * SL.Global.ActiveModifiers.MusicRate)..GenerateBreakdownText(pn, 0))
 			local minimization_level = 1
 			while self:GetWidth() > (width/textZoom) and minimization_level < 4 do
-				self:settext(GenerateBreakdownText(pn, minimization_level))
+				self:settext(("Peak NPS: %.1f   "):format(SL[pn].Streams.PeakNPS * SL.Global.ActiveModifiers.MusicRate)..GenerateBreakdownText(pn, minimization_level))
 				minimization_level = minimization_level + 1
 			end
 		end,
