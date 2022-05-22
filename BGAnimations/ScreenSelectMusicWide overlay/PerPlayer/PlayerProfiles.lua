2f0835534551b89730a360bebe82d5c8d1181a20
Horsey <cheval.de.jeanvaljean@gmail.com>
2022-05-22 00:55:40 -0700

    Expand ScreenSelectMusicWide player profiles
    ScreenSelectMusicWide now shows a LOT more information about the player profiles that are active.
    
    The player profile card now displays more than just Star counts and how many times a player attempted the current song. I really wanted to include caloric workout data here, but after looking into how the game calculates calories burned, I don't think that the game's calorie calculations are accurate enough to warrant placing the information front and center. I would love it if the game engine had hooks to directly communicate with more accurate external fitness monitors, but as of now this just isn't the case. I couldn't even find a way to set goal calories...? The APIs are there, but it's not even implemented in this theme as far as I can tell.
    
    New information included:
    
    -Total Songs completed
    -Speed mod *** (see note below)
    -Scroll Rate***
    -Time played in Gameplay in the current set
    -Number of songs played (attempted) in the current set
    -Steps hit in the current set
    -Lifetime amount of time in Gameplay
    -Lifetime amount of steps hit
    
    ***Hopefully in the future I will implement changing of speed mods while still on ScreenSelectMusicWide. The speed mod information probably isn't necessary at this time, but I think it's helpful overall. It'll make more sense when I can eventually implement this.

--- BGAnimations/ScreenSelectMusicWide overlay/PerPlayer/PlayerProfiles.lua
+++ BGAnimations/ScreenSelectMusicWide overlay/PerPlayer/PlayerProfiles.lua
@@ -10,8 +10,129 @@ local tristars = profile:GetTotalScoresWithGrade('Grade_Tier02')
 local doublestars = profile:GetTotalScoresWithGrade('Grade_Tier03')
 local singlestars = profile:GetTotalScoresWithGrade('Grade_Tier04')
 
-local w = 266.5
-local h = 84
+local w = 268
+local h = 208
+local offset = 63
+
+--used to calculate the scroll speed with respect to rate mod, speed mod type, BPM ranges all respective to each player
+--this was copied from ScreenPlayerOptions overlay/default.lua
+--this function will eventually need to be updated to accomodate the new speed mod types supported by OutFox
+
+local CalculateScrollSpeed = function(player)
+	player   = player or GAMESTATE:GetMasterPlayerNumber()
+	local pn = ToEnumShortString(player)
+
+	local Steps = GAMESTATE:GetCurrentSong()
+	local MusicRate    = SL.Global.ActiveModifiers.MusicRate or 1
+
+	local SpeedModType = SL[pn].ActiveModifiers.SpeedModType
+	local SpeedMod     = SL[pn].ActiveModifiers.SpeedMod
+
+	local bpms = GetDisplayBPMs(player, Steps, MusicRate)
+	if not (bpms and bpms[1] and bpms[2]) then return "" end
+
+	if SpeedModType=="X" then
+		bpms[1] = bpms[1] * SpeedMod
+		bpms[2] = bpms[2] * SpeedMod
+
+	elseif SpeedModType=="M" then
+		bpms[1] = bpms[1] * (SpeedMod/bpms[2])
+		bpms[2] = SpeedMod
+
+	elseif SpeedModType=="C" then
+		bpms[1] = SpeedMod
+		bpms[2] = SpeedMod
+	end
+
+	-- format as strings
+	bpms[1] = ("%.0f"):format(bpms[1])
+	bpms[2] = ("%.0f"):format(bpms[2])
+
+	if bpms[1] == bpms[2] then
+		return bpms[1]
+	end
+
+	return ("%s-%s"):format(bpms[1], bpms[2])
+end
+
+
+--this code was taken from ScreenGameOver overlay/PlayerStatsWithoutProfile.lua
+--this code will calculate Steps Hit during the session and for how long a session lasted
+local totalTime = 0
+local songsPlayedThisGame = 0
+local notesHitThisGame = 0
+
+-- Use pairs here (instead of ipairs) because this player might have late-joined
+-- which will result in nil entries in the the Stats table, which halts ipairs.
+-- We're just summing total time anyway, so order doesn't matter.
+for i,stats in pairs( SL[ToEnumShortString(player)].Stages.Stats ) do
+	totalTime = totalTime + (stats and stats.duration or 0)
+	songsPlayedThisGame = songsPlayedThisGame + (stats and 1 or 0)
+
+	if stats and stats.column_judgments then
+		-- increment notesHitThisGame by the total number of tapnotes hit in this particular stepchart by using the per-column data
+		-- don't rely on the engine's non-Miss judgment counts here for two reasons:
+		-- 1. we want jumps/hands to count as more than 1 here
+		-- 2. stepcharts can have non-1 #COMBOS parameters set which would artbitraily inflate notesHitThisGame
+
+		for column, judgments in ipairs(stats.column_judgments) do
+			for judgment, judgment_count in pairs(judgments) do
+				if judgment ~= "Miss" then
+					notesHitThisGame = notesHitThisGame + judgment_count
+				end
+			end
+		end
+	end
+end
+
+local hours = math.floor(totalTime/3600)
+local minutes = math.floor((totalTime-(hours*3600))/60)
+local seconds = round(totalTime%60)
+
+--These local fuctions are used for displaying a player's currently selected mods; this may be useful in the future
+--Until there's a way to change these mods from ScreenSelectMusicWide I see no reason to display this information since SpeedMods are really the only mods that players change from song to song
+
+-- local PlayerOptions = GAMESTATE:GetPlayerState(player):GetPlayerOptionsArray("ModsLevel_Preferred")
+-- -- start with an empty string...
+-- local optionslist = ""
+--
+-- -- if the player used an XMod of 1x, it won't be in PlayerOptions list
+-- -- so check here, and add it in manually if necessary
+-- if SL[pn].ActiveModifiers.SpeedModType == "X" and SL[pn].ActiveModifiers.SpeedMod == 1 then
+-- 	optionslist = "1x, "
+-- end
+--
+-- --  ...and append options to that string as needed
+-- for i,option in ipairs(PlayerOptions) do
+--
+-- 	-- these don't need to show up in the mods list
+-- 	if option ~= "FailAtEnd" and option ~= "FailImmediateContinue" and option ~= "FailImmediate" then
+-- 		-- 100% Mini will be in the PlayerOptions as just "Mini" so use the value from the SL table instead
+-- 		if option:match("Mini") then
+-- 			option = SL[pn].ActiveModifiers.Mini .. " Mini"
+-- 		end
+--
+-- 		if option:match("Cover") then
+-- 			option = THEME:GetString("OptionNames", "Cover")
+-- 		end
+--
+-- 		if i < #PlayerOptions then
+-- 			optionslist = optionslist..option..", "
+-- 		else
+-- 			optionslist = optionslist..option
+-- 		end
+-- 	end
+-- end
+
+
+-- a string representing the NoteSkin the player was using
+
+-- local noteskin = GAMESTATE:GetPlayerState(player):GetCurrentPlayerOptions():NoteSkin()
+
+-- NOTESKIN:LoadActorForNoteSkin() expects the noteskin name to be all lowercase(?)
+-- so transform the string to be lowercase
+
+-- noteskin = noteskin:lower()
 -----
 
 
@@ -43,7 +164,7 @@ return Def.ActorFrame{
 
 	InitCommand=function(self)
 		self:visible( false ):halign( p )
-		self:y(SCREEN_TOP + 74)
+		self:y(SCREEN_TOP + 73)
 
 		if player == PLAYER_1 then
 			self:x( _screen.cx - 294)
@@ -60,14 +181,16 @@ return Def.ActorFrame{
   --black outline
   Def.Quad{
       InitCommand = function(self)
-          self:zoomto(w, h):diffuse(Color.Black)
+          self:zoomto(w, h):diffuse(Color.Black):diffusealpha(0.65)
+					self:y(offset)
       end
   },
   --background quad
   Def.Quad{
       InitCommand = function(self)
           self:zoomto(w - 2, h - 2)
-          self:diffuse(color("#1e282f"))
+          self:diffuse(color("#1e282f")):diffusealpha(0.65)
+					self:y(offset)
       end
   },
   --Profile Name
@@ -79,22 +202,16 @@ return Def.ActorFrame{
 				self:settext(profile_name)
       end,
   },
-  --Number of times current song played
+
+	--Total Number Of Songs Completed
   LoadFont("Common Normal")..{
-      Text = "Number Times Song Played",
-			CurrentSongChangedMessageCommand=function(self) self:playcommand("Set") end,
+      Text = "Total Songs Completed",
       InitCommand = function(self)
 				self:horizalign(center)
 				self:xy(42,-5)
 				self:zoom(0.8)
+				self:settext("Total Songs Completed: ".. commify(profile:GetNumTotalSongsPlayed()))
       end,
-			SetCommand=function(self)
-				if GAMESTATE:GetCurrentSong() == nil then
-					self:settext("")
-				else
-					self:settext("Times Played: "..PROFILEMAN:GetSongNumTimesPlayed(GAMESTATE:GetCurrentSong(),p))
-				end
-			end,
   },
 
 	-- Profile photo
@@ -165,7 +282,7 @@ return Def.ActorFrame{
 			InitCommand = function(self)
 				self:horizalign(center)
 				self:xy(-25,32)
-				self:settext(quads)
+				self:settext(commify(quads))
 				self:zoom(0.7):maxwidth(40)
 			end,
 	},
@@ -185,7 +302,7 @@ return Def.ActorFrame{
 			InitCommand = function(self)
 				self:horizalign(center)
 				self:xy(18.5,32)
-				self:settext(tristars)
+				self:settext(commify(tristars))
 				self:zoom(0.7)
 			end,
 	},
@@ -205,7 +322,7 @@ return Def.ActorFrame{
 			InitCommand = function(self)
 				self:horizalign(center)
 				self:xy(62,32)
-				self:settext(doublestars)
+				self:settext(commify(doublestars))
 				self:zoom(0.7)
 			end,
 	},
@@ -225,8 +342,272 @@ return Def.ActorFrame{
 			InitCommand = function(self)
 				self:horizalign(center)
 				self:xy(105,32)
-				self:settext(singlestars)
+				self:settext(commify(singlestars))
 				self:zoom(0.7)
 			end,
 	},
+
+	-- thin white line separating stats from mods
+	Def.Quad {
+		InitCommand=function(self)
+			self:zoomto(w-20,2):xy(0,47):diffusealpha(0.5)
+		end,
+	},
+
+	--Number of times current song attemped
+	LoadFont("Common Normal")..{
+		Text = "Number Times Song Attempted",
+		CurrentSongChangedMessageCommand=function(self) self:playcommand("Set") end,
+		InitCommand = function(self)
+			self:horizalign(center)
+			self:y(59)
+			self:zoom(0.8)
+		end,
+		SetCommand=function(self)
+			if GAMESTATE:GetCurrentSong() == nil then
+				self:settext("")
+			else
+				self:settext("Times Song Attempted: "..commify(PROFILEMAN:GetSongNumTimesPlayed(GAMESTATE:GetCurrentSong(),p)))
+			end
+		end,
+	},
+
+	--Speed Mod Label
+	LoadFont("Common Normal")..{
+			Text = "SPEED MOD",
+			InitCommand = function(self)
+				self:horizalign(left)
+				self:xy(-127,77)
+				self:zoom(0.7)
+				self:diffuse(color("0.5,0.5,0.5,1"))
+			end,
+	},
+
+	--Speed Mod Value
+  LoadFont("Common Normal")..{
+    Text = "Speed Mod",
+    InitCommand = function(self)
+			self:horizalign(left)
+			self:xy(-73,77)
+			self:zoom(0.7)
+			self:queuecommand("Set")
+    end,
+		CurrentSongChangedMessageCommand=function(self) self:playcommand("Set") end,
+		SetCommand=function(self)
+			if GAMESTATE:GetCurrentSong() == nil then
+				self:settext("")
+			else
+				self:settext(SL[pn].ActiveModifiers.SpeedModType..SL[pn].ActiveModifiers.SpeedMod)
+			end
+		end,
+  },
+
+	--Scroll Rate Label
+	LoadFont("Common Normal")..{
+			Text = "SCROLL RATE",
+			InitCommand = function(self)
+				self:horizalign(right)
+				self:xy(127,77)
+				self:zoom(0.7)
+				self:diffuse(color("0.5,0.5,0.5,1"))
+			end,
+	},
+
+	--Scroll Rate Value
+  LoadFont("Common Normal")..{
+    Text = "Scroll Rate",
+    InitCommand = function(self)
+			self:horizalign(right)
+			self:xy(65,77)
+			self:zoom(0.7)
+			self:queuecommand("Set")
+    end,
+		CurrentSongChangedMessageCommand=function(self) self:playcommand("Set") end,
+		SetCommand=function(self)
+			if GAMESTATE:GetCurrentSong() == nil then
+				self:settext("")
+			else
+				self:settext(("%s%s"):format(SL[pn].ActiveModifiers.SpeedModType, CalculateScrollSpeed(player)))
+			end
+		end,
+  },
+
+	-- thin white line separating mods from profile stats
+	Def.Quad {
+		InitCommand=function(self)
+			self:zoomto(w-20,2):xy(0,90):diffusealpha(0.5)
+		end,
+	},
+
+--in Pay Modes, I understand that these set metrics might not make sense to most users
+--even though the data shown would be per-set and not per-day, I think that's overall valuable to the player and should not be removed in pay modes
+
+	--Time Spent In Gameplay Label
+	LoadFont("Common Normal")..{
+			Text = "TIME PLAYED",
+			InitCommand = function(self)
+				self:horizalign(center)
+				self:xy(-89,102)
+				self:zoom(0.7)
+				self:diffuse(color("0.5,0.5,0.5,1"))
+			end,
+	},
+
+	--Time Spent In Gameplay Value
+  LoadFont("Common Normal")..{
+    Text = "TimePlayedThisSetValue",
+    InitCommand = function(self)
+			self:horizalign(center)
+			self:xy(-89,116)
+			self:zoom(0.7)
+			if hours > 1 then
+				self:settext(hours .. " hrs, " .. minutes .. " mins")
+			elseif hours == 1 then
+				self:settext(hours .. " hr, " .. minutes .. " mins")
+			else
+				self:settext(minutes .. " mins")
+			end
+		end,
+  },
+
+	--Songs Played This Set Label
+	LoadFont("Common Normal")..{
+			Text = "PLAYED THIS SET",
+			InitCommand = function(self)
+				self:horizalign(center)
+				self:y(102)
+				self:zoom(0.7)
+				self:diffuse(color("0.5,0.5,0.5,1"))
+			end,
+	},
+
+	--Songs Played This Set Value
+  LoadFont("Common Normal")..{
+    Text = "SongsPlayedThisSetValue",
+    InitCommand = function(self)
+			self:horizalign(center)
+			self:y(116)
+			self:zoom(0.7)
+			if songsPlayedThisGame == 1 then
+				self:settext(songsPlayedThisGame.." Song")
+			else
+				self:settext(songsPlayedThisGame.." Songs")
+			end
+		end,
+  },
+
+	--Steps Hit This Set Label
+	LoadFont("Common Normal")..{
+			Text = "STEPS HIT",
+			InitCommand = function(self)
+				self:horizalign(center)
+				self:xy(89,102)
+				self:zoom(0.7)
+				self:diffuse(color("0.5,0.5,0.5,1"))
+			end,
+	},
+
+	--Steps Hit This Set Value
+  LoadFont("Common Normal")..{
+    Text = "notesHitThisGameValue",
+    InitCommand = function(self)
+			self:horizalign(center)
+			self:xy(89,116)
+			self:zoom(0.7)
+			self:settext(commify(notesHitThisGame))
+		end,
+  },
+
+--lifetime stats
+
+	--Time Spent In Gameplay Label (Lifetime)
+	LoadFont("Common Normal")..{
+			Text = "LIFETIME PLAYED",
+			InitCommand = function(self)
+				self:horizalign(center)
+				self:xy(-51,138)
+				self:zoom(0.7)
+				self:diffuse(color("0.5,0.5,0.5,1"))
+			end,
+	},
+		--Time Spent In Gameplay (Lifetime)
+		LoadFont("Common Normal")..{
+			Text = "LifetimeGameplayValue",
+			InitCommand = function(self)
+				local Lifetime = profile:GetTotalGameplaySeconds()
+				local TotalDays = math.floor(Lifetime/86400)
+			  local TotalHours = math.floor(math.fmod(Lifetime, 86400)/3600)
+			  local TotalMinutes = math.floor(math.fmod(Lifetime,3600)/60)
+				self:horizalign(center)
+				self:xy(-51,152)
+				self:zoom(0.7)
+				--lots of rules here to handle the gramatically correct way of displaying time data (in English)
+				--I geniunely think that the format DDD:HH:MM is less optimal to English-speaking players
+				--FIXME:there should probably be a check for English vs other languages and just display DDD:HH:MM instead, but until there is a need for other regions, I'm keeping things as-is
+
+				--to sum up the rules: show [ days + hours /or/ hours + minutes /or/ minutes ]
+				--unfortunately, displaying [days + hours + minutes] takes up an unreasonable amount of space in this iteration of the UI
+
+				--I highly doubt there will be many people who play with profiles with a large number of days worth of playtime to make the jump to "weeks + days + hours" make sense
+				--a large number of days takes up less space than jumping to weeks/years calculations to the point that it's unreasonable for a player to have played long engough where displaying "weeks/years" will save space and/or make more sense
+				--my hope is that once a player has achieved years worth of ingame playtime this game will have moved to a better way to display this sort of information
+
+				if TotalDays > 1 and TotalHours == 1 then
+					self:settext(TotalDays .. " days, " .. TotalHours .. " hr")
+				elseif TotalDays > 1 and TotalHours ~= 1 then
+					self:settext(TotalDays .. " days, " .. TotalHours .. " hrs")
+				elseif TotalDays == 1 and TotalHours == 1 then
+					self:settext(TotalDays .. " day, " .. TotalHours .. " hr")
+				elseif TotalDays == 1 and TotalHours ~= 1 then
+					self:settext(TotalDays .. " day, " .. TotalHours .. " hrs")
+				elseif TotalDays <= 0 and TotalHours > 1 and TotalMinutes > 1 then
+					self:settext(TotalHours .. " hrs, " .. TotalMinutes .. " mins")
+				elseif TotalDays <= 0 and TotalHours > 1 and TotalMinutes == 0 then
+					self:settext(TotalHours .. " hrs, " .. TotalMinutes .. " mins")
+				elseif TotalDays <= 0 and TotalHours > 1 and TotalMinutes == 1 then
+					self:settext(TotalHours .. " hrs, " .. TotalMinutes .. " min")
+				elseif TotalDays <= 0 and TotalHours == 1 and TotalMinutes == 1 then
+					self:settext(TotalHours .. " hr, " .. TotalMinutes .. " min")
+				elseif TotalDays <= 0 and TotalHours == 1 and TotalMinutes ~= 1 then
+					self:settext(TotalHours .. " hr, " .. TotalMinutes .. " mins")
+				elseif TotalDays <= 0 and TotalHours <= 0 and TotalMinutes ~= 1 then
+					self:settext(TotalMinutes .. " mins")
+				elseif TotalDays <= 0 and TotalHours <= 0 and TotalMinutes == 1 then
+					self:settext(TotalMinutes .. " min")
+				end
+			end,
+		},
+
+		--Lifetime Steps Label
+		LoadFont("Common Normal")..{
+				Text = "LIFETIME STEPS",
+				InitCommand = function(self)
+					self:horizalign(center)
+					self:xy(89-38,138)
+					self:zoom(0.7)
+					self:diffuse(color("0.5,0.5,0.5,1"))
+				end,
+		},
+
+		--Lifetime Steps Value
+	  LoadFont("Common Normal")..{
+	    Text = "NotesHitLifetimeValue",
+	    InitCommand = function(self)
+				self:horizalign(center)
+				self:xy(89-38,152)
+				self:zoom(0.7)
+				self:settext(commify(profile:GetTotalTapsAndHolds()))
+			end,
+	  },
+
+	--NoteSkin prieview
+
+	--can't be used because loads at first as the default noteskin then after going to player mods and exiting out or going to GAMEPLAY will correctly display the player's chosen NoteSkin
+	-- GetNoteSkinActor() is defined in ./Scripts/SL-Helpers.lua, and performs some
+	-- rudimentary error handling because NoteSkins From The Internet™ may contain Lua errors
+	-- LoadActor(THEME:GetPathB("","_modules/NoteSkinPreview.lua"), {noteskin_name=noteskin})..{
+	-- 	OnCommand=function(self)
+	-- 		self:xy(-100-5-2+3,76):zoom(0.55):visible(true)
+	-- 	end
+	-- },
 }
